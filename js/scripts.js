e6474c184ea8a858b6099f37727dcb14feab136f
Corey Redding and Kristen McKee <corey+kristen@>
2017-05-09 14:21:07 -0700

    add constructor and prototype that shows user input after user submits their selections

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,31 @@
+function priceOutput(movie, time, age) {
+  this.movie = movie;
+  this.time = time;
+  this.age = age;
+  this.prices = [];
+}
+
+priceOutput.prototype.fullPrice = function() {
+  return this.movie + ", " + this.time + ", " + this.age;
+}
+
+
+
+
+//// Front End Logic ////
 $(document).ready(function() {
 
-var movie = $("#movie").val();
+  $("form#ticketForm").submit(function(event) {
+    event.preventDefault();
+
+  var movie = $("#movie").val();
+  var time = $("#time").val();
+  var age = $("#age").val();
+  var price = new priceOutput(movie, time, age);
+
+  $("ul#price").append(price.fullPrice());
+
+  });
+
 
 });
